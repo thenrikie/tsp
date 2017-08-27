eb9c92e4c5161c850f9bda5a07fc0b90ee62bfd5
Henry Liu <thenrikie@gmail.com>
2017-08-28 00:41:07 +0100

    add sortDestinations function and modify unit tests for it

---
+++ google-map/sortDestinations.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const GOOGLE_DISTANCE_API_URL = 'https://maps.googleapis.com/maps/api/distancematrix/json';
+const request = require('request-promise');
+const _ = require('lodash');
+
+/** 
+ * use google distance matrix to get distance and then sort dests by distance
+ * @param {Array} origin - origin in [lat, long]
+ * @param {Array} destinations - array of destionations in [lat, long]
+ * @return {Promise} order of destinations visitng for shortest distance
+ */
+
+function sortDestinations(origin, destinations){
+	
+	const opts = {
+		uri: GOOGLE_DISTANCE_API_URL,
+		qs: {
+			origins: origin.join(','),
+			destinations: destinations.map(dest => dest.join(',')).join('|')
+		},
+		json: true
+	};
+
+	return request(opts).then(data => {
+		return _sortPoints(data);
+	});
+}
+
+/** 
+ * helper function to sort destinations 
+ * @param {Array} data - data return from google api
+ * @return {Array} order of destinations visitng for shortest distance
+ */
+
+function _sortPoints(data){
+
+	// make sure data.rows[0].elements exists
+	const elements = _.get(data, 'rows[0].elements', []);
+
+	// throw error if there any of the status is not OK
+	if(elements.find(e => e.status !== 'OK')){
+		throw { message: 'Some destinations cannot be reached'}
+	}
+
+	// make an array of { oriIndex, distance } and sort it by distance
+	let sortedDests = elements.map((value, index) => {
+		return {
+			orgIndex: index,
+			distance: value.distance.value
+		}
+	});
+
+	sortedDests = sortedDests.sort((a, b) => a.distance - b.distance);
+
+	//return optimized order of destinations for visiting
+	return sortedDests.map(dest => dest.orgIndex);
+}
+
+module.exports = {
+	sortDestinations,
+	// export for unit testing
+	_sortPoints
+};
--- test/sortPoints.js
+++ test/sortPoints.js
@@ -8,45 +8,132 @@ describe('sortPoints', () => {
 
 	it('should return empty array if no destinations provided', () => {
 
-		const dests = [];
+		const dests = {rows:[]};
 		expect(sortPoints(dests)).to.deep.equal([]);
 	});
 
 	it('should return [0] if there is only 1 destination', () => {
 
-		const dests = [
-			{distance: { value: 3 }}
-		];
+		const dests = {
+			rows: [{
+				elements: [{
+					distance: {
+						value: 3,
+					},
+					status: 'OK'
+				}]
+			}]
+		};
+
 		expect(sortPoints(dests)).to.deep.equal([0]);
 	});
 
 	it('should sort objects by its distance in asc order (same order)', () => {
 
-		const dests = [
-			{distance: { value: 3 }},
-			{distance: { value: 4 }},
-			{distance: { value: 5 }},
-		];
+		const dests = {
+			rows: [{
+				elements: [{
+					distance: {
+						value: 3,
+					},
+					status: 'OK'
+				},
+				{
+					distance: {
+						value: 4,
+					},
+					status: 'OK'
+				},
+				{
+					distance: {
+						value: 5,
+					},
+					status: 'OK'
+				}]
+			}]
+		};
+
 		expect(sortPoints(dests)).to.deep.equal([0,1,2]);
 	});
 
 	it('should sort objects by its distance in asc order (desc order)', () => {
 
-		const dests = [
-			{distance: { value: 5 }},
-			{distance: { value: 4 }},
-			{distance: { value: 3 }},
-		];
+		const dests = {
+			rows: [{
+				elements: [{
+					distance: {
+						value: 5,
+					},
+					status: 'OK'
+				},
+				{
+					distance: {
+						value: 4,
+					},
+					status: 'OK'
+				},
+				{
+					distance: {
+						value: 3,
+					},
+					status: 'OK'
+				}]
+			}]
+		};
+
 		expect(sortPoints(dests)).to.deep.equal([2,1,0]);
 	});
 
 	it('should sort objects by its distance in asc order (mixed order)', () => {
 
-		const dests = [
-			{distance: { value: 5 }},
-			{distance: { value: 4 }},
-			{distance: { value: 6 }},
-		];
+		const dests = {
+			rows: [{
+				elements: [{
+					distance: {
+						value: 5,
+					},
+					status: 'OK'
+				},
+				{
+					distance: {
+						value: 4,
+					},
+					status: 'OK'
+				},
+				{
+					distance: {
+						value: 6,
+					},
+					status: 'OK'
+				}]
+			}]
+		};
+
 		expect(sortPoints(dests)).to.deep.equal([1,0,2]);
+	});
+
+	it('should throw an error if any of the element is not OK', () => {
+
+		const dests = {
+			rows: [{
+				elements: [{
+					status: 'NOT_FOUND'
+				},
+				{
+					distance: {
+						value: 4,
+					},
+					status: 'OK'
+				},
+				{
+					distance: {
+						value: 6,
+					},
+					status: 'OK'
+				}]
+			}]
+		};
+
+		expect(() => sortPoints(dests)).to.throw();
 	});
 });
