2ff6fa95bae9d12ecdc47a3d11741e916989a5fd
Henry Liu <thenrikie@gmail.com>
2017-08-29 15:06:39 +0100

    update getRouteInfo function and update unit test for it

---
+++ google-map/getRouteInfo.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const GOOGLE_DIRECTION_API_URL = 'https://maps.googleapis.com/maps/api/directions/json';
+const request = require('request-promise');
+const _ = require('lodash');
+
+/** 
+ * use google direction API to time and distance info
+ * @param {Array} origin - origin in [lat, long]
+ * @param {Array} waypoints - array of [lat, long] for intermediate drop off points
+ * @param {Array} destination - destionation in [lat, long]
+ * @return {Promise} time and distance information { time, distance }
+ */
+
+
+function getRouteInfo(origin, waypoints, destination){
+	
+	const opts = {
+		uri: GOOGLE_DIRECTION_API_URL,
+		qs: {
+			origin: origin.join(','),
+			waypoints: waypoints.map(dest => dest.join(',')).join('|'),
+			destination: destination.join(','),
+		},
+		json: true
+	};
+
+	return request(opts).then(data => {
+		return _calRouteInfo(data);
+	});
+}
+
+/** 
+ * helper function to calualte total distance and time need for a route
+ * @param {Array} data - data return from google api
+ * @return {Object}
+ */
+function _calRouteInfo(data){
+	// handle top level error from google api
+	if(!data || data.status !== 'OK'){
+		throw { message: 'Google API Error', googleAPIStatus: _.get(data, 'status') };
+	}
+
+	//use only first route provided by the google api and assume it is the one of the best route
+	const legs = _.get(data, 'routes[0].legs');
+
+	if(!Array.isArray(legs) || legs.length === 0){
+		throw { message: 'No legs information'}
+	}
+
+	return legs.reduce((sum, leg) => {
+
+		sum.distance += leg.distance.value,
+		sum.duration += leg.duration.value
+
+		return sum;
+
+	}, { distance: 0, duration: 0});
+
+}
+
+module.exports = { 
+	getRouteInfo, 
+	_calRouteInfo
+};
--- test/calRouteInfo.js
+++ test/calRouteInfo.js
@@ -6,22 +6,17 @@ const calRouteInfo = require('../google-map/getRouteInfo')._calRouteInfo;
 
 describe('calRouteInfo', () => {
 
-	it('should return empty object if no destinations provided', () => {
-
-		const dests = {rows:[], status: 'OK'};
-		expect(calRouteInfo(dests)).to.deep.equal({});
-
-	});
-
 	it('should return object with correct distance and time info if one leg info is provided', () => {
 
 		const dests = {
-			legs:[
-				{
-					distance: { value: 300 },
-					duration: { value: 20 }
-				}
-			], 
+			routes: [{
+				legs:[
+					{
+						distance: { value: 300 },
+						duration: { value: 20 }
+					}
+				]
+			}],
 			status: 'OK'
 		};
 
@@ -32,20 +27,22 @@ describe('calRouteInfo', () => {
 	it('should return object with correct distance and time info if more than one legs are provided', () => {
 
 		const dests = {
-			legs:[
-				{
-					distance: { value: 300 },
-					duration: { value: 20 }
-				},
-				{
-					distance: { value: 100 },
-					duration: { value: 10 }
-				},
-				{
-					distance: { value: 222 },
-					duration: { value: 77 }
-				}
-			], 
+			routes: [{
+				legs:[
+					{
+						distance: { value: 300 },
+						duration: { value: 20 }
+					},
+					{
+						distance: { value: 100 },
+						duration: { value: 10 }
+					},
+					{
+						distance: { value: 222 },
+						duration: { value: 77 }
+					}
+				],
+			}],
 			status: 'OK'
 		};
 		
@@ -56,7 +53,6 @@ describe('calRouteInfo', () => {
 	it('should throw an error there is if any top level error', () => {
 
 		const dests = {
-			rows: [],
 			status: 'OVER_QUERY_LIMIT'
 		};
 
@@ -66,5 +62,14 @@ describe('calRouteInfo', () => {
 		});
 	});
 
+	it('should throw an error if no destinations provided', () => {
+
+		const dests = {routes:[{legs:[]}], status: 'OK'};
+
+		expect(() => calRouteInfo(dests)).to.throw().to.deep.equal({ 
+			message: 'No legs information', 
+		});
+
+	});
 });
 
